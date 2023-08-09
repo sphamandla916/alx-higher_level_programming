41dabd99bb26d1863120b09d36fa55eee917bc85
sphamandla916 <siphamandlamashego29@gmail.com>
2023-08-09 20:34:32 +0200

    Fixed the sys error

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin//python3
-from sys.stderr import write
 import sys
-
-write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
 sys.exit(1)
